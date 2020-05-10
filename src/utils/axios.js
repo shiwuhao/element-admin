e6711be60967adfe9bd963366bf3a11ed1481fec
shiwuhao <shiwuhao@shiwuhao.com>
2020-05-10 10:53:03 +0800

    axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,39 +3,65 @@ import Qs from 'qs';
 import {getToken} from '@/utils/auth';
 
 const instance = axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
-  headers: {Accept: 'text/json'},
+    baseURL: process.env.VUE_APP_API_URL,
+    headers: {Accept: 'text/json'},
 });
 
 // 请求拦截
 instance.interceptors.request.use(function (config) {
 
+    if (config.method === "post") {
+        config.data = Qs.stringify(config.data);
+        config.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+    }
+    if (getToken()) {
+        config.headers['Authorization'] = 'Bearer ' + getToken();
+    }
 
-  if (config.method === "post") {
-    config.data = Qs.stringify(config.data);
-    config.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-  }
-  if (getToken()) {
-    config.headers['Authorization'] = 'Bearer ' + getToken();
-  }
-
-  return config;
+    return config;
 }, function (error) {
-  return Promise.reject(error);
+    return Promise.reject(error);
 });
 
 // 响应拦截
 instance.interceptors.response.use((response) => {
 
 
-  return response;
+    return response;
 }, (err) => {
-  console.table(err.config.url);
+    console.table(err.config.url);
+    if (err && err.response) {
 
-  if (err && err.response) {
-    console.log(err);
-  }
-  return Promise.reject(err)
+        let noticeConfig = {};
+
+        switch (err.response.status) {
+            case 400:
+
+                break;
+            case 403:
+                noticeConfig = {title: 403, desc: err.response.data.message, duration: 0};
+                break;
+            case 401:
+                if (err.config.url.indexOf('login') > 0) {
+                    noticeConfig = {title: 401, desc: err.response.data.message, duration: 0};
+                } else {
+
+                }
+                break;
+            case 404:
+                noticeConfig = {title: 404, desc: err.response.data.message, duration: 0};
+                break;
+            case 422:
+
+                break;
+            default:
+                noticeConfig = {title: err.response.status, desc: err.response.data.message};
+        }
+        if (Object.keys(noticeConfig).length > 0) {
+
+        }
+    }
+    return Promise.reject(err)
 });
 
 export default instance;
