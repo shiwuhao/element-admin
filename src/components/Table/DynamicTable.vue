9b8bef2665e2f06d326b1c36535051fa1bb7bbec
石武浩 <shiwuhao@juzifenqi.com>
2020-07-27 20:35:21 +0800

    table

--- src/components/Table/DynamicTable.vue
+++ src/components/Table/DynamicTable.vue
@@ -1,20 +1,41 @@
 <template>
   <el-table border :data="data" tooltip-effect="light" style="width: 100%;">
-    <el-table-column
-      v-for="(value,key,index) in columns"
-      :prop="key"
-      :key="index"
-      :index="value.index"
-      :column-key="value.columnKey"
-      :label="value.label"
-      :width="value.width"
-      :min-width="value.minWidth"
-      :fixed="value.fixed"
-      :render-header="value.renderHeader"
-      :sortable="value.sortable"
-      :sort-method="value.sortMethod"
-      :sort-by="value.sortBy"
-    />
+    <template v-for="(value,key,index) in columns">
+      <el-table-column
+        v-if="!value.slot"
+        :prop="key"
+        :key="index"
+        :index="value.index"
+        :column-key="value.columnKey"
+        :label="value.label"
+        :width="value.width"
+        :min-width="value.minWidth"
+        :fixed="value.fixed"
+        :render-header="value.renderHeader"
+        :sortable="value.sortable"
+        :sort-method="value.sortMethod"
+        :sort-by="value.sortBy">
+      </el-table-column>
+      <el-table-column
+        v-else
+        :prop="key"
+        :key="index"
+        :index="value.index"
+        :column-key="value.columnKey"
+        :label="value.label"
+        :width="value.width"
+        :min-width="value.minWidth"
+        :fixed="value.fixed"
+        :render-header="value.renderHeader"
+        :sortable="value.sortable"
+        :sort-method="value.sortMethod"
+        :sort-by="value.sortBy">
+        <template slot-scope="scope">
+          <slot :name="value.slot" :row="scope.row" :$index="scope.$index"/>
+        </template>
+      </el-table-column>
+    </template>
+    <slot></slot>
   </el-table>
 </template>
 
